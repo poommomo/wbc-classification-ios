ded3c1da81e8ac2713dcb4a354122dbd264492c3
poommomo <perlmomo@gmail.com>
2018-11-24 23:38:40 +0700

    Initial Commit

---
+++ WBC Classification/DetectionHistoryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetectionHistoryViewController.swift
+//  WBC Classification
+//
+//  Created by Poom Penghiran on 24/11/2561 BE.
+//  Copyright © 2561 Poom Penghiran. All rights reserved.
+//
+
+import UIKit
+
+class DetectionHistoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
